f3fed0030ec749e8afe83475b1536574b948b532
jhss <mtvfc@naver.com>
2023-02-12 23:32:25 +0900

    add missing files

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230212.225441';
+const cacheName = 'chirpy-20230212.233112';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
